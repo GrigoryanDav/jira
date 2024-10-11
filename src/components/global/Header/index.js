1da9166d1fd9606df56b418bfa0eb4929c356625
GrigoryanDav <grigoryand219@gmail.com>
2024-10-11 16:32:24 +0400

    Implement routing for Sign In button in header

--- src/components/global/Header/index.js
+++ src/components/global/Header/index.js
@@ -1,3 +1,5 @@
+import { ROUTE_CONSTANTS } from "../../../core/utils/constants";
+import { Link } from "react-router-dom";
 import { Flex, Button } from "antd";
 import './index.css'
 
@@ -6,11 +8,15 @@ const Header = () => {
     return (
         <div className="main_header">
             <Flex justify="space-between" align="center">
-            <p>Logo</p>
+                <p>Logo</p>
 
-            <div>
-                <Button>Sign In</Button>
-            </div>
+                <div>
+                    <Link to={ROUTE_CONSTANTS.LOGIN}>
+                        <Button>
+                            Sign in
+                        </Button>
+                    </Link>
+                </div>
             </Flex>
         </div>
     )
