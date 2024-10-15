1b4a738586c84cf71921e276c27cca5d24cb391d
GrigoryanDav <grigoryand219@gmail.com>
2024-10-15 19:18:25 +0400

    add cabinet page and routing using by react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,18 +2,30 @@ import { Register, Login } from "./pages/auth"
 import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom'
 import { ROUTE_CONSTANTS } from "./core/utils/constants"
 import MainLayout from './components/layouts/Main'
+import Cabinet from "./pages/cabinet"
+import { auth } from "./services/firebase"
+import { onAuthStateChanged } from "firebase/auth"
+import { useEffect, useState } from "react"
 import './styles/global.css'
 
 
 const App = () => {
+    const [isAuth, setIsAuth] = useState(false)
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            setIsAuth(Boolean(user))
+        });
+    }, []);
     return (
-        <RouterProvider 
+        <RouterProvider
             router={
                 createBrowserRouter(
                     createRoutesFromElements(
                         <Route path="/" element={<MainLayout />}>
                             <Route path={ROUTE_CONSTANTS.LOGIN} element={<Login />} />
                             <Route path={ROUTE_CONSTANTS.REGISTER} element={<Register />} />
+                            <Route path={ROUTE_CONSTANTS.CABINET} element={<Cabinet />} />
                         </Route>
                     )
                 )
