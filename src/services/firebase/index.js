5605c973d92d5c1962240045204019acc5b1084b
GrigoryanDav <grigoryand219@gmail.com>
2024-10-23 18:16:07 +0400

    feat: integrate Firestore and clean up comments

--- src/services/firebase/index.js
+++ src/services/firebase/index.js
@@ -1,12 +1,8 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-//import { getAnalytics } from "firebase/analytics";
-import {getAuth} from 'firebase/auth'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
   apiKey: "AIzaSyBNup9HfLih21a8CtP3PGQQu3flt2GWh7g",
   authDomain: "jira-4aca0.firebaseapp.com",
@@ -18,10 +14,13 @@ const firebaseConfig = {
   measurementId: "G-6DZYC7VTDD"
 };
 
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
 const auth = getAuth(app)
 
 export {
-    auth
+  db,
+  auth,
 }
