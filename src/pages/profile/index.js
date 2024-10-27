bc2d7f9356ad4ebce3351531489a8d5267aa6ff1
GrigoryanDav <grigoryand219@gmail.com>
2024-10-27 21:55:13 +0400

    feat(profile): redesign and improve component

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,11 +1,62 @@
+import { Button, Form, Input } from 'antd'
+import { AuthContext } from '../../context/authContext'
+import { useContext, useEffect } from 'react'
 import './index.css'
 
 
 
 const Profile = () => {
+    const { userProfileInfo } = useContext(AuthContext)
+    const [form] = Form.useForm()
+
+    useEffect(() => {
+        const { uid, ...restData } = userProfileInfo
+        form.setFieldsValue(restData)
+    }, [form, userProfileInfo])
+
     return (
         <div>
-            <h2>Profile</h2>
+            <Form layout='vertical' form={form}>
+                <Form.Item
+                    label="First Name"
+                    name='firstName'
+                >
+                    <Input
+                        placeholder='First Name'
+                    />
+                </Form.Item>
+
+                <Form.Item
+                    label='Last Name'
+                    name='lastName'
+                >
+                    <Input
+                        placeholder='Last Name'
+                    />
+                </Form.Item>
+
+                <Form.Item
+                    label='Email'
+                    name='email'
+                >
+                    <Input
+                        placeholder='Email'
+                    />
+                </Form.Item>
+
+                <Form.Item
+                    label='Phone Number'
+                    name='phoneNumber'
+                >
+                    <Input
+                        placeholder='Phone Number'
+                    />
+                </Form.Item>
+
+                <Button type='primary' htmlType='submit'>
+                    Submit
+                </Button>
+            </Form>
         </div>
     )
 }
