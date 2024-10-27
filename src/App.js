6c90e0a1fe95e4cd3bcc941ae038008ddfaa3f66
GrigoryanDav <grigoryand219@gmail.com>
2024-10-27 22:03:40 +0400

    Update route constants and nesting for Cabinet/Profile

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route, N
 import { ROUTE_CONSTANTS, FIRESTORE_PATH_NAMES } from "./core/utils/constants"
 import MainLayout from './components/layouts/Main'
 import LoadingWrapper from "./components/sheard/LoadingWrapper"
-import Cabinet from "./pages/cabinet"
 import Profile from "./pages/profile"
+import CabinetLayout from "./components/layouts/Cabinet"
 import { doc, getDoc } from 'firebase/firestore'
 import { auth, db } from "./services/firebase"
 import { onAuthStateChanged } from "firebase/auth"
@@ -32,7 +32,7 @@ const App = () => {
         const docRef = doc(db, FIRESTORE_PATH_NAMES.REGISTERED_USERS, uid)
         const response = await getDoc(docRef)
 
-        if(response.exists()) {
+        if (response.exists()) {
             setUserProfileInfo(response.data())
         }
     }
@@ -47,8 +47,16 @@ const App = () => {
                                 <Route path="/" element={<MainLayout />}>
                                     <Route path={ROUTE_CONSTANTS.LOGIN} element={isAuth ? <Navigate to={ROUTE_CONSTANTS.CABINET} /> : <Login />} />
                                     <Route path={ROUTE_CONSTANTS.REGISTER} element={isAuth ? <Navigate to={ROUTE_CONSTANTS.CABINET} /> : <Register />} />
-                                    <Route path={ROUTE_CONSTANTS.CABINET} element={isAuth ? <Cabinet /> : <Navigate to={ROUTE_CONSTANTS.LOGIN} />} />
-                                    <Route path={ROUTE_CONSTANTS.PROFILE} element={isAuth ? <Profile /> : <Navigate to={ROUTE_CONSTANTS.LOGIN} />} />
+
+                                    <Route
+                                        path={ROUTE_CONSTANTS.CABINET}
+                                        element={isAuth ? <CabinetLayout /> : <Navigate to={ROUTE_CONSTANTS.LOGIN} />}
+                                    >
+                                        <Route
+                                            path={ROUTE_CONSTANTS.PROFILE}
+                                            element={<Profile />}
+                                        />
+                                    </Route>
                                 </Route>
                             )
                         )
