0da0b4546d02e925a8b817c19461e31d44d5cf2e
GrigoryanDav <grigoryand219@gmail.com>
2024-11-06 17:59:40 +0400

    Update App.js to integrate Redux and connect with Redux store

--- src/App.js
+++ src/App.js
@@ -1,44 +1,28 @@
 import { Register, Login } from "./pages/auth"
 import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route, Navigate } from 'react-router-dom'
-import { ROUTE_CONSTANTS, FIRESTORE_PATH_NAMES } from "./core/utils/constants"
+import { ROUTE_CONSTANTS } from "./core/utils/constants"
 import MainLayout from './components/layouts/Main'
 import LoadingWrapper from "./components/sheard/LoadingWrapper"
 import Profile from "./pages/profile"
 import CabinetLayout from "./components/layouts/Cabinet"
-import { doc, getDoc } from 'firebase/firestore'
-import { auth, db } from "./services/firebase"
-import { onAuthStateChanged } from "firebase/auth"
-import { useEffect, useState, useCallback } from "react"
-import { AuthContext } from "./context/authContext"
+import { useEffect } from "react"
+import { useSelector, useDispatch } from "react-redux"
+import { fetchUserProfileInfo } from "./state-managment/slices/userProfile"
 import './styles/global.css'
 
 
 
+
 const App = () => {
-    const [isAuth, setIsAuth] = useState(false)
-    const [loading, setLoading] = useState(true)
-    const [userProfileInfo, setUserProfileInfo] = useState({})
-
-    const handleGetUserData = useCallback(async (uid) => {
-        const docRef = doc(db, FIRESTORE_PATH_NAMES.REGISTERED_USERS, uid)
-        const response = await getDoc(docRef)
-
-        if (response.exists()) {
-            setUserProfileInfo(response.data())
-        }
-    }, [])
+    const { loading, authUserInfo: { isAuth } } = useSelector((store) => store.userProfile)
+    const dispatch = useDispatch()
 
     useEffect(() => {
-        onAuthStateChanged(auth, (user) => {
-            user?.uid && handleGetUserData(user.uid)
+        dispatch(fetchUserProfileInfo())
+    }, [dispatch])
 
-            setLoading(false)
-            setIsAuth(Boolean(user))
-        });
-    }, [handleGetUserData]);
 
     return (
-        <AuthContext.Provider value={{ isAuth, userProfileInfo, handleGetUserData }}>
             <LoadingWrapper loading={loading}>
                 <RouterProvider
                     router={
@@ -63,7 +47,6 @@ const App = () => {
                     }
                 />
             </LoadingWrapper>
-        </AuthContext.Provider>
     )
 }
 
