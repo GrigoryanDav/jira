5764afec7fb90eeb764f9522fc39b8b7eda15b15
GrigoryanDav <grigoryand219@gmail.com>
2024-12-05 15:51:57 +0400

    Add task list with editable modal functionality

--- src/pages/cabinet/index.js
+++ src/pages/cabinet/index.js
@@ -1,9 +1,19 @@
 import { Button } from "antd"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import AddIssueModal from "../../components/sheard/IssueModal/Add"
+import EditIssueModal from "../../components/sheard/IssueModal/Edit"
+import { useSelector, useDispatch } from "react-redux"
+import { fetchIssuesData } from "../../state-managment/slices/issues"
 
 const Cabinet = () => {
+    const dispatch = useDispatch()
     const [showModal, setShowModal] = useState(false)
+    const { data, isLoading } = useSelector((store) => store.issues)
+    const [editModalData, setEditModalData] = useState(null)
+
+    useEffect(() => {
+        dispatch(fetchIssuesData())
+    }, [dispatch])
 
     const handleOpenModal = () => {
         setShowModal(true)
@@ -19,7 +29,31 @@ const Cabinet = () => {
                 Create Issue
             </Button>
 
+            {
+                Boolean(editModalData) && (
+                    <EditIssueModal 
+                        isOpen={Boolean(editModalData)}
+                        data={editModalData}
+                        onClose={() => setEditModalData(null)}
+                    />
+                )
+            }
+
             <AddIssueModal onClose={handleClose} isOpen={showModal} />
+
+            <div>
+                <ul>
+                    {
+                        data.map((item) => {
+                            return (
+                                <li  key={item.taskId} onClick={() => setEditModalData(item)}>
+                                    {item.issueName}
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+            </div>
         </div>
     )
 }
